2a6c4d359c839136051b7270bb7cb51d5231a448
Neil Guo <kissme3131@gmail.com>
2021-07-26 21:48:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="tsparticles">
+  </div>
+  <div class="banner">
+   reqeqweqwk 
+  </div>
+  <img alt="vue logo" src="./assets/logo.png">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { tsParticles } from 'tsparticles';
+export default defineComponent({
+  name: 'App',
+  mounted() {
+    tsParticles
+    .loadJSON("tsparticles", "static/particles.json")
+    .then((container) => {
+      console.log("callback - tsparticles config loaded" + container);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  }
+});
+</script>
+
+<style>
+#app {
+  margin: 0;
+}
+#tsparticles {
+  z-index: -10px;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: #323840;
+}
+.banner{
+  width: 300px;
+  height: 100vh;
+  background-color: white;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <img alt="vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+  z-index: 1;
+  width: 200px;
+  background-color: #ffff;
+  margin-top: 100px;
+  height: 1000px;
+
+}
+</style>
